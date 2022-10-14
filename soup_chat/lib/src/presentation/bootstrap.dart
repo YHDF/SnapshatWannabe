71b0222406c6729a7ecd665735cfb1cef523f3b8
YHDF <unes.houdaifa@gmail.com>
2022-10-14 14:33:01 +0200

    FEAT: Refacto project structure #2

---
+++ soup_chat/lib/src/presentation/bootstrap.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Bootstrap extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => BootstrapState();
+}
+
+class BootstrapState extends State<Bootstrap> {
+  //final List<Widget> _tabItems = [Home(), Classement(), UserProfile(),];
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+  int _activePage = 0;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.amberAccent,
+        selectedLabelStyle: TextStyle(color: Colors.black),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
