1b4bfb61d1d257bec428d85766c4a4072f1b7ef6
YHDF <unes.houdaifa@gmail.com>
2022-10-14 13:58:31 +0200

    FEAT: Refacto project structure

--- soup_chat/lib/src/main.dart
+++ soup_chat/lib/src/main.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:soup_chat/data/entities/ricknmortycharacters.dart';
+
+import 'data/entities/ricknmortycharacters.dart';
 void main() {
   runApp(const MyApp());
 }
