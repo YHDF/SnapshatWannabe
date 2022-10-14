09dbb02663c55759a5a284ea1fc47921921c5e83
mustakhl <musta.khalouk@gmail.com>
2022-10-14 14:51:13 +0200

    Configuration firebase with flutter

--- soup_chat/lib/src/main.dart
+++ soup_chat/lib/src/main.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:soup_chat/src/presentation/bootstrap.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
